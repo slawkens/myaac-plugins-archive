3f7f172a947f8ba7056688f0a49dd0a1f1daec18
slawkens <slawkens@gmail.com>
2023-05-25 17:45:21 +0200

    google-recaptcha-v1.0: fix v3 recaptcha loading

--- google-recaptcha-v1.0/plugins/google-recaptcha/hooks/admin-head-body.php
+++ google-recaptcha-v1.0/plugins/google-recaptcha/hooks/admin-head-body.php
@@ -4,7 +4,7 @@ defined('MYAAC') or die('Direct access not allowed!');
 $configRecaptcha = config('google_recaptcha');
 if(getBoolean($configRecaptcha['enabled'])) {
 	if ($this->_type == HOOK_ADMIN_HEAD_END) {
-		echo '<script src="https://www.google.com/recaptcha/api.js' . ($configRecaptcha['type'] == 'v3' ? 'render=' . config('recaptcha_site_key') : '') . '"></script>';
+		echo '<script src="https://www.google.com/recaptcha/api.js' . ($configRecaptcha['type'] == 'v3' ? '?render=' . config('recaptcha_site_key') : '') . '"></script>';
 	}
 	else if($this->_type == HOOK_ADMIN_BODY_END) {
 		$twig->display('google-recaptcha/templates/recaptcha-v3.html.twig', [
--- google-recaptcha-v1.0/plugins/google-recaptcha/hooks/before-page.php
+++ google-recaptcha-v1.0/plugins/google-recaptcha/hooks/before-page.php
@@ -11,7 +11,7 @@ if(!isset($template_place_holders['head_end'])) {
 $configRecaptcha = config('google_recaptcha');
 if(getBoolean($configRecaptcha['enabled'])) {
 	// insert into page head
-	$template_place_holders['head_end'][] = '<script src="https://www.google.com/recaptcha/api.js' . ($configRecaptcha['type'] == 'v3' ? 'render=' . config('recaptcha_site_key') : '') . '"></script>';
+	$template_place_holders['head_end'][] = '<script src="https://www.google.com/recaptcha/api.js' . ($configRecaptcha['type'] == 'v3' ? '?render=' . config('recaptcha_site_key') : '') . '"></script>';
 
 	if ($configRecaptcha['type'] == 'v3') {
 		$template_place_holders['body_end'][] = $twig->render('google-recaptcha/templates/recaptcha-v3.html.twig', [
